1ac027cf45d0edc32342fc79c89f25f6fc072465
John Trehearn <johntrehearnbc@gmail.com>
2024-09-17 11:56:32 +0300

    single product formated

--- js/products.js
+++ js/products.js
@@ -9,6 +9,10 @@ class Products {
             url: this.apiUrl + 'products/' + id,
             success: function (data) {
                 console.log(data);
+                $('.product_image').html('<img src="' + data.image + '" class="img-fluid">');
+                $('.product_title').html(data.title);
+                $('.product_price').html("£" + data.price.toFixed(2));  
+                $('.product_description').html("<p>" + data.description + "</p>");
             },
            
         });
